63d1afef31888120d5d276a78ab3e2f2d39e5a5b
YtMa <ytma98@gmail.com>
2025-12-02 22:56:02 -0800

    Day 1 b

--- src/day1.rs
+++ src/day1.rs
@@ -5,7 +5,7 @@ fn day1_a(input: &str) -> u32 {
     for rotation in input.lines() {
         let rotation = parse_rotation(rotation);
         pos += rotation;
-        pos %= 100;
+        pos = pos.rem_euclid(100);
         if pos == 0 {
             zero_count += 1;
         }
@@ -22,6 +22,30 @@ fn parse_rotation(rotation: &str) -> i32 {
         'R' => num,
         _ => panic!("Invalid rotation: {}", rotation),
     }
+}
+
+fn day1_b(input: &str) -> u32 {
+    let mut pos = 50;
+    let mut crossings = 0;
+
+    for rotation in input.lines() {
+        let rotation = parse_rotation(rotation);
+        let crossings_change = count_zero_crossings(pos, rotation);
+        crossings += crossings_change;
+        pos += rotation;
+        pos = pos.rem_euclid(100);
+    }
+
+    crossings
+}
+
+// We count landing at 0 as a crossing
+// But not when starting at 0
+fn count_zero_crossings(pos: i32, rotation: i32) -> u32 {
+    let offset = if rotation >= 0 { 0 } else { 1 };
+    let end = (pos + rotation - offset).div_euclid(100);
+    let start = (pos - offset).div_euclid(100);
+    (end - start).abs() as u32
 }
 
 #[cfg(test)]
@@ -48,5 +72,31 @@ mod tests {
     fn test_day1_a_real() {
         let input = include_str!("../data/day1_a_real.txt");
         println!("{}", day1_a(input));
+    }
+
+    #[test]
+    fn test_count_zero_crossings() {
+        assert_eq!(count_zero_crossings(50, -68), 1);
+        assert_eq!(count_zero_crossings(52, 48), 1);
+        assert_eq!(count_zero_crossings(95, 60), 1);
+        assert_eq!(count_zero_crossings(55, -55), 1);
+        assert_eq!(count_zero_crossings(99, -99), 1);
+        assert_eq!(count_zero_crossings(14, -82), 1);
+        assert_eq!(count_zero_crossings(50, 1000), 10);
+        assert_eq!(count_zero_crossings(82, -30), 0);
+        assert_eq!(count_zero_crossings(52, 48), 1);
+        assert_eq!(count_zero_crossings(0, 10), 0);
+    }
+
+    #[test]
+    fn test_day1_b_example() {
+        let input = include_str!("../data/day1_a_example.txt");
+        assert_eq!(day1_b(input), 6);
+    }
+
+    #[test]
+    fn test_day1_b_real() {
+        let input = include_str!("../data/day1_a_real.txt");
+        println!("{}", day1_b(input));
     }
 }
