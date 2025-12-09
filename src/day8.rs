4e2fbcf85c4cb7ce42a678dda18b6d0164a6bba9
YtMa <ytma98@gmail.com>
2025-12-08 22:23:04 -0800

    Clean up day 8

--- src/day8.rs
+++ src/day8.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::hash::Hash;
 
 pub fn run() {
     let input = include_str!("../data/day8_real.txt");
@@ -14,39 +13,58 @@ struct Position {
     z: i64,
 }
 
-fn part1(input: &str, iterations: usize) -> u64 {
-    let positions = parse_input(input);
-    let mut disjoint_set = DisjointSet::new(&positions);
-
-    let mut edges = Vec::new();
-    for (i, pos1) in positions.iter().enumerate() {
-        for (j, pos2) in positions.iter().enumerate() {
-            if i >= j {
-                continue;
-            }
+fn compute_edges(positions: &[Position]) -> Vec<(i64, Position, Position)> {
+    let mut edges = Vec::with_capacity(positions.len() * (positions.len() - 1) / 2);
+    for (i, &pos1) in positions.iter().enumerate() {
+        for &pos2 in &positions[i + 1..] {
             let dx = pos1.x - pos2.x;
             let dy = pos1.y - pos2.y;
             let dz = pos1.z - pos2.z;
             let dist = dx * dx + dy * dy + dz * dz;
-            edges.push((dist, *pos1, *pos2));
+            edges.push((dist, pos1, pos2));
         }
     }
+    edges.sort_unstable_by_key(|e| e.0);
+    edges
+}
 
-    edges.sort_by_key(|e| e.0);
+fn part1(input: &str, iterations: usize) -> u64 {
+    let positions = parse_input(input);
+    let mut disjoint_set = DisjointSet::new(&positions);
+    let edges = compute_edges(&positions);
 
-    for (_, p1, p2) in edges.iter().take(iterations) {
-        disjoint_set.union(*p1, *p2);
+    for (_, p1, p2) in edges.into_iter().take(iterations) {
+        disjoint_set.union(p1, p2);
     }
 
     let mut sizes = disjoint_set.sizes();
-    sizes.sort_unstable_by_key(|s| *s);
-    sizes.reverse();
-    
-    if sizes.len() < 3 {
-        return 0;
+    sizes.sort_unstable_by(|a, b| b.cmp(a));
+
+    sizes
+        .get(0..3)
+        .map(|top3| top3.iter().map(|&s| s as u64).product())
+        .unwrap_or(0)
+}
+
+fn part2(input: &str) -> u64 {
+    let positions = parse_input(input);
+    let mut disjoint_set = DisjointSet::new(&positions);
+    let edges = compute_edges(&positions);
+
+    let mut last_pair = (0, 0);
+    let mut components = positions.len();
+
+    for (_, pos1, pos2) in edges {
+        if disjoint_set.union(pos1, pos2) {
+            last_pair = (pos1.x, pos2.x);
+            components -= 1;
+            if components == 1 {
+                break;
+            }
+        }
     }
 
-    sizes[0] as u64 * sizes[1] as u64 * sizes[2] as u64
+    (last_pair.0.abs() as u64) * (last_pair.1.abs() as u64)
 }
 
 struct DisjointSet {
@@ -56,17 +74,13 @@ struct DisjointSet {
 
 impl DisjointSet {
     fn new(nodes: &[Position]) -> Self {
-        let mut parent = HashMap::new();
-        let mut rank = HashMap::new();
-        for pos in nodes {
-            parent.insert(*pos, *pos);
-            rank.insert(*pos, 0);
-        }
+        let parent = nodes.iter().map(|&p| (p, p)).collect();
+        let rank = nodes.iter().map(|&p| (p, 0)).collect();
         Self { parent, rank }
     }
 
     fn find(&mut self, x: Position) -> Position {
-        let p = *self.parent.get(&x).unwrap_or(&x);
+        let p = self.parent[&x];
         if p != x {
             let root = self.find(p);
             self.parent.insert(x, root);
@@ -76,82 +90,55 @@ impl DisjointSet {
         }
     }
 
-    fn union(&mut self, x: Position, y: Position) {
+    /// Returns true if union was performed (nodes were in different sets)
+    fn union(&mut self, x: Position, y: Position) -> bool {
         let x_root = self.find(x);
         let y_root = self.find(y);
         if x_root == y_root {
-            return;
+            return false;
         }
-        let x_rank = *self.rank.get(&x_root).unwrap_or(&0);
-        let y_rank = *self.rank.get(&y_root).unwrap_or(&0);
 
-        if x_rank < y_rank {
-            self.parent.insert(x_root, y_root);
-        } else if x_rank > y_rank {
-            self.parent.insert(y_root, x_root);
-        } else {
-            self.parent.insert(y_root, x_root);
-            self.rank.insert(x_root, x_rank + 1);
+        let x_rank = self.rank[&x_root];
+        let y_rank = self.rank[&y_root];
+
+        match x_rank.cmp(&y_rank) {
+            std::cmp::Ordering::Less => {
+                self.parent.insert(x_root, y_root);
+            }
+            std::cmp::Ordering::Greater => {
+                self.parent.insert(y_root, x_root);
+            }
+            std::cmp::Ordering::Equal => {
+                self.parent.insert(y_root, x_root);
+                *self.rank.get_mut(&x_root).unwrap() += 1;
+            }
         }
-    }
-
-    fn connected(&mut self, x: Position, y: Position) -> bool {
-        self.find(x) == self.find(y)
+        true
     }
 
     fn sizes(&mut self) -> Vec<usize> {
-        let mut sizes = HashMap::new();
         let positions: Vec<Position> = self.parent.keys().copied().collect();
+        let mut sizes: HashMap<Position, usize> = HashMap::new();
         for pos in positions {
             let root = self.find(pos);
             *sizes.entry(root).or_insert(0) += 1;
         }
-        sizes.values().cloned().collect()
+        sizes.into_values().collect()
     }
-}
-
-
-fn part2(input: &str) -> u64 {
-    let positions = parse_input(input);
-    let mut disjoint_set = DisjointSet::new(&positions);
-
-    let mut edges = Vec::new();
-    for (i, pos1) in positions.iter().enumerate() {
-        for (j, pos2) in positions.iter().enumerate() {
-            if i >= j {
-                continue;
-            }
-            let dx = pos1.x - pos2.x;
-            let dy = pos1.y - pos2.y;
-            let dz = pos1.z - pos2.z;
-            let dist = dx * dx + dy * dy + dz * dz;
-            edges.push((dist, *pos1, *pos2));
-        }
-    }
-
-    edges.sort_by_key(|e| e.0);
-
-    let mut last_x1_x2 = (0, 0);
-    let mut components = positions.len();
-    for &(_dist, pos1, pos2) in &edges {
-        if !disjoint_set.connected(pos1, pos2) {
-            disjoint_set.union(pos1, pos2);
-            last_x1_x2 = (pos1.x, pos2.x);
-            components -= 1;
-            if components == 1 {
-                break;
-            }
-        }
-    }
-
-    (last_x1_x2.0.abs() as u64) * (last_x1_x2.1.abs() as u64)
 }
 
 fn parse_input(input: &str) -> Vec<Position> {
-    input.lines().map(|line| {
-        let [x, y, z] = line.split(',').map(|s| s.parse().unwrap()).collect::<Vec<i64>>().try_into().unwrap();
-        Position { x, y, z }
-    }).collect()
+    input
+        .lines()
+        .filter_map(|line| {
+            let mut parts = line.split(',').map(|s| s.parse::<i64>().ok());
+            Some(Position {
+                x: parts.next()??,
+                y: parts.next()??,
+                z: parts.next()??,
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
